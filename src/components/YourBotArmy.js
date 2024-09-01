d2849298eaf79a1496bb7ca7d8ce6ef7de5a9cdd
Wato Elle <watoelle@gmail.com>
2024-09-01 22:51:19 +0300

    updated

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,33 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
-  //your bot army code here...
+function YourBotArmy({ bots, detachBot, deleteBot }) {
+  const enlistedClasses = [];
+
+  const handleEnlist = (bot) => {
+    if (!enlistedClasses.includes(bot.bot_class)) {
+      enlistedClasses.push(bot.bot_class);
+      detachBot(bot);
+    }
+  };
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+          {bots.length > 0 ? (
+            bots.map(bot => (
+              <BotCard
+                key={bot.id}
+                bot={bot}
+                clickEvent={() => detachBot(bot)}
+                deleteBot={() => deleteBot(bot)}
+                onEnlist={() => handleEnlist(bot)}
+              />
+            ))
+          ) : (
+            <div className="column">No bots in your army</div>
+          )}
         </div>
       </div>
     </div>
@@ -16,3 +35,6 @@ function YourBotArmy() {
 }
 
 export default YourBotArmy;
+
+
+
